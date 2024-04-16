eddd8ca59059535aa4dc9e9af3b6c3bd5db5ec8f
ricovaniy <115656372+ricovaniy@users.noreply.github.com>
2024-04-16 19:06:27 +0500

    abubebababubu 6-7

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 let formCount = 1;
+function highlightKeywords(text) {
+    const keywords = ['срочно', 'быстрее', 'побыстрее', 'скорее', 'поскорее', 'очень нужно'];
+    const regex = new RegExp(keywords.join('|'), 'gi');
+    return text.replace(regex, match => `<b>${match}</b>`);
+}
+document.querySelector('textarea').addEventListener('input', (e) => {
+    document.querySelector('.anything').innerHTML = highlightKeywords(document.querySelector('textarea').value);
+})
 document.querySelector('.add-button').addEventListener('click', () => {
     formCount++;
     let forms = document.querySelectorAll(".beverage");
@@ -18,6 +26,11 @@ function resetField(form) {
         checkbox.checked = false;
     });
     form.appendChild(createButton(form))
+    form.querySelector('.anything').innerHTML = '';
+    form.querySelector('textarea').textContent = '';
+    form.querySelector('textarea').addEventListener('input', (e) => {
+        form.querySelector('.anything').innerHTML = highlightKeywords(form.querySelector('textarea').value);
+    })
 }
 
 function createButton(form) {
@@ -32,6 +45,7 @@ function createButton(form) {
     });
     return button;
 }
+
 function getDeclension(count) {
     count = Math.abs(count);
     count %= 100;
@@ -47,12 +61,73 @@ function getDeclension(count) {
     }
     return 'напитков';
 }
+
 const submitButton = document.querySelector('.submit-button');
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeButton = document.querySelector('.close');
+
+function appendBRBRBBRBRBBRBRBRBRBRBRB(element) {
+    element.appendChild(document.createElement("br"));
+}
+
 submitButton.addEventListener('click', function () {
     modalOverlay.style.display = 'flex';
-    modalOverlay.querySelector('.modal p').textContent += `\nВы заказали ${formCount} ${getDeclension(formCount)}.`;
+    const paragraph = modalOverlay.querySelector('.modal p');
+    appendBRBRBBRBRBBRBRBRBRBRBRB(paragraph);
+    paragraph.textContent += `\nВы заказали ${formCount} ${getDeclension(formCount)}.`;
+    const modal = modalOverlay.querySelector('.modal');
+    appendBRBRBBRBRBBRBRBRBRBRBRB(modal);
+    modal.appendChild(document.createElement('table'));
+    const table = modal.querySelector('table');
+    const head = document.createElement('thead');
+    head.appendChild(document.createElement('tr'));
+    const h1 = document.createElement('th');
+    h1.textContent = 'Напиток';
+    head.querySelector('tr').appendChild(h1);
+    const h2 = document.createElement('th');
+    h2.textContent = 'Молоко';
+    head.querySelector('tr').appendChild(h2);
+    const h3 = document.createElement('th');
+    h3.textContent = 'Дополнительно';
+    head.querySelector('tr').appendChild(h3);
+    table.appendChild(head);
+    const body = document.createElement('tbody');
+    const forms = document.querySelectorAll('fieldset');
+    forms.forEach((form) => {
+        const tr = document.createElement('tr');
+        const td = document.createElement('td');
+        const dropDown = form.querySelector('select');
+        td.textContent = dropDown.selectedOptions[0].value;
+        tr.appendChild(td);
+        const td1 = document.createElement('td');
+        const radioButtons = form.querySelectorAll('input[type=radio]');
+
+
+        radioButtons.forEach((radio) => {
+            if (radio.checked) {
+                const labelText = radio.value;
+                td1.textContent = labelText;
+            }
+        });
+        tr.appendChild(td1);
+        const td2 = document.createElement('td');
+        const checkboxes = form.querySelectorAll('input[type=checkbox]');
+
+
+        checkboxes.forEach((radio) => {
+            if (radio.checked) {
+                const labelText = radio.value;
+                if (td2.textContent.length === 0) {
+                    td2.textContent = labelText;
+                } else {
+                    td2.textContent += ', ' + labelText;
+                }
+            }
+        });
+        tr.appendChild(td2);
+        body.appendChild(tr);
+    })
+    table.appendChild(body);
 });
 closeButton.addEventListener('click', function () {
     modalOverlay.style.display = 'none';
